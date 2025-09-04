6cc79d9f005fe518b58b6b97fbcfcc453ece6ca1
Felipe Mendoza <felipe188.mendoza@gmail.com>
2025-09-04 00:15:04 -0500

    variables de entorno

--- db/connection.go
+++ db/connection.go
@@ -1,17 +1,35 @@
 package db
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DSN = "host=localhost user=postgres password=chepita dbname=bd_sis_operativos port=5432 TimeZone=America/Lima"
+var DSN = ""
 var DB *gorm.DB
 var err error
 
 func DBConnection() {
+
+	_ = godotenv.Load()
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	timezone := os.Getenv("DB_TIMEZONE")
+
+	DSN = fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s TimeZone=%s",
+		host, user, password, dbname, port, timezone,
+	)
+
 	DB, err = gorm.Open(postgres.Open(DSN), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
